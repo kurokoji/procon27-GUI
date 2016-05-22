f54a3c48497ec119c69abc88867c0caec86188ff
kurokoji <shu.kakihana@gmail.com>
2016-05-22 11:14:15 +0900

    First Commit フレーム,ピース情報の読み込み書き込み

---
+++ WakuAndPiece/WAP.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace WakuAndPiece {
+  partial class WakuAndPiece {
+    /// <summary>
+    /// 必要なデザイナー変数です。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 使用中のリソースをすべてクリーンアップします。
+    /// </summary>
+    /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows フォーム デザイナーで生成されたコード
+
+    /// <summary>
+    /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+    /// コード エディターで変更しないでください。
+    /// </summary>
+    private void InitializeComponent() {
+      this.readFramePiece = new System.Windows.Forms.Button();
+      this.outputSolve = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // readFramePiece
+      // 
+      this.readFramePiece.Location = new System.Drawing.Point(36, 52);
+      this.readFramePiece.Name = "readFramePiece";
+      this.readFramePiece.Size = new System.Drawing.Size(110, 33);
+      this.readFramePiece.TabIndex = 0;
+      this.readFramePiece.Text = "読み込み";
+      this.readFramePiece.UseVisualStyleBackColor = true;
+      this.readFramePiece.Click += new System.EventHandler(this.readFramePiece_Click);
+      // 
+      // outputSolve
+      // 
+      this.outputSolve.Location = new System.Drawing.Point(36, 104);
+      this.outputSolve.Name = "outputSolve";
+      this.outputSolve.Size = new System.Drawing.Size(110, 33);
+      this.outputSolve.TabIndex = 1;
+      this.outputSolve.Text = "Solverに渡す";
+      this.outputSolve.UseVisualStyleBackColor = true;
+      this.outputSolve.Click += new System.EventHandler(this.outputSolve_Click);
+      // 
+      // WakuAndPiece
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1188, 596);
+      this.Controls.Add(this.outputSolve);
+      this.Controls.Add(this.readFramePiece);
+      this.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+      this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+      this.Name = "WakuAndPiece";
+      this.Text = "WAP";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button readFramePiece;
+    private System.Windows.Forms.Button outputSolve;
+  }
+}
+
---
+++ WakuAndPiece/WAP.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WakuAndPiece {
+  using Piece = Polygon;
+  using Hole = Polygon;
+
+  public partial class WakuAndPiece : Form {
+    public WakuAndPiece() {
+      InitializeComponent();
+    }
+
+    Problem problem;
+
+    /* フレーム,ピース情報の読み込み */
+    private void readFramePiece_Click(object sender, EventArgs e) {
+      /*
+      Process readQuestprocess = new Process();
+      readQuestprocess.StartInfo.FileName = "";
+      readQuestprocess.StartInfo.UseShellExecute = false;
+      readQuestprocess.StartInfo.RedirectStandardOutput = true;
+      // 読み込みプロセスの開始
+      readQuestprocess.Start();
+      */
+
+      /* 今はテキストから読み込んでるだけ 5/22 */
+
+      // フレーム,ピース情報を読み込む
+      StreamReader questReader = new StreamReader("quest.txt"); //readFrameprocess.StandardOutput;
+      problem = Problem.fromStream(questReader);
+      questReader.Close();
+    }
+
+    /* ソルバーにフレーム,ピース情報を出力 */
+    private void outputSolve_Click(object sender, EventArgs e) {
+      /*
+      Process writePicAnalysisprocess = new Process();
+      writePicAnalysisprocess.StartInfo.FileName = "";
+      writePicAnalysisprocess.StartInfo.UseShellExecute = false;
+      writePicAnalysisprocess.StartInfo.RedirectStandardInput = true;
+      writePicAnalysisprocess.Start();
+      */
+
+      /* 今はテキストに書き込んでるだけ */
+
+      // フレーム,ピース情報を書き込む
+      StreamWriter toPicAnalysisWriter = new StreamWriter("toPic.txt"); // writePicAnalysisprocess.StandardOutput
+      problem.toStream(toPicAnalysisWriter);
+      toPicAnalysisWriter.Close();
+    }
+  }
+  
+  // 座標
+  class Vertex {
+    public double X { get; }
+    public double Y { get; }
+    // X,Yに値をセットするコンストラクタ
+    public Vertex(double x, double y) {
+      X = x;
+      Y = y;
+    }
+  }
+
+  // 図形(フレームの穴とピースに使われる)
+  class Polygon {
+    public Vertex[] vertices { get; }
+    // Vertexクラスをセットするコンストラクタ
+    public Polygon(Vertex[] vertices) {
+      this.vertices = vertices;
+    }
+
+    // StreamReaderから読み込む
+    public static Polygon fromStream(StreamReader sr) {
+      // 要素数を入力
+      int N = int.Parse(sr.ReadLine());
+      // 要素数分だけ確保
+      Vertex[] vertices = new Vertex[N];
+      for (int i = 0; i < N; i++) {
+        // 座標を入力
+        string[] tokens = sr.ReadLine().Split(' ');
+        double x = double.Parse(tokens[0]);
+        double y = double.Parse(tokens[1]);
+        vertices[i] = new Vertex(x, y);
+      }
+      return (new Polygon(vertices));
+    }
+    public void toStream(StreamWriter sw) {
+      // 要素数を出力
+      sw.WriteLine(vertices.Length);
+      for (int i = 0; i < vertices.Length; i++) {
+        // 座標を出力
+        sw.WriteLine("{0} {1}", vertices[i].X, vertices[i].Y);
+      }
+    }
+  }
+
+  // フレーム
+  class Frame {
+    public Hole[] holes { get; }
+    // 穴情報をセットするコンストラクタ
+    public Frame(Hole[] holes) {
+      this.holes = holes;
+    }
+
+    // StreamReaderから読み込む
+    public static Frame fromStream(StreamReader sr) {
+      // 要素数の入力
+      int N = int.Parse(sr.ReadLine());
+      // 要素数分だけ確保
+      Hole[] holes = new Hole[N];
+      for (int i = 0; i < N; i++) {
+        // StreamReaderを渡し,HoleのfromStream内で座標を読み込む
+        holes[i] = Hole.fromStream(sr);
+      }
+      return (new Frame(holes));
+    }
+
+    // Streamに出力
+    public void toStream(StreamWriter sw) {
+      // 穴の数を書き込む
+      sw.WriteLine(holes.Length);
+      for (int i = 0; i < holes.Length; i++) {
+        holes[i].toStream(sw);
+      }   
+    }
+  }
+
+  class Problem {
+    public Frame frame { get; }
+    public Piece[] pieces { get; }
+    // フレーム情報とピース情報をセットするコンストラクタ
+    private Problem(Frame frame, Piece[] pieces) {
+      this.frame = frame;
+      this.pieces = pieces;
+    }
+    // Streamから各情報を読み取る
+    public static Problem fromStream(StreamReader sr) {
+      Frame frame = Frame.fromStream(sr);
+      // 要素数の入力
+      int N = int.Parse(sr.ReadLine());
+      // 要素数分だけ確保
+      Piece[] pieces = new Piece[N];
+      for (int i = 0; i < N; i++) {
+        pieces[i] = Piece.fromStream(sr);
+      }
+      return (new Problem(frame, pieces));
+    }
+
+    // Streamに出力
+    public void toStream(StreamWriter sw) {
+      frame.toStream(sw);
+      // ピースの数を出力
+      sw.WriteLine(pieces.Length);
+      for (int i = 0; i < pieces.Length; i++) {
+        pieces[i].toStream(sw);
+      }
+    }
+  }
+}
