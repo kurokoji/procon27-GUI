c56efe8deb628ad7f8a51b69392ce8c84d693695
kurokoji <shu.kakihana@gmail.com>
2016-08-17 17:51:56 +0900

    ID修正がある場合は修正してからpiecelistに描画

--- WakuAndPiece/WAP.cs
+++ WakuAndPiece/WAP.cs
@@ -86,12 +86,13 @@ namespace WakuAndPiece {
         // フレーム,ピース情報を読み込む
         problem = Problem.fromStream(readQuestreader.StandardOutput);
       }
-      // リストへの描画
-      problem.showpieceList(pieceListpanel);
       // ID被りがあったピースが1個以上あればID変更フォームを表示
       if (problem.misspieces.Count > 0) {
         ChangeID changeid = new ChangeID(problem, pieceListpanel);
         changeid.Show();
+      } else {
+        // リストへの描画
+        problem.showpieceList(pieceListpanel);
       }
     }
     
